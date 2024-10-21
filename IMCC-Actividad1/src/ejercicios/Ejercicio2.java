9b3db3b99c8e5378305a0bdde6d1c2167963e8c4
Casandra Iglesias <cciglesiasmartinez@gmail.com>
2024-10-21 06:10:52 +0200

    Completed exercise 2

--- IMCC-Actividad1/src/ejercicios/Ejercicio2.java
+++ IMCC-Actividad1/src/ejercicios/Ejercicio2.java
@@ -1,10 +1,46 @@
 package ejercicios;
 
+/**
+
+ * Diseñar un programa que muestre el producto y la suma de los 10 primeros 
+ * números impares.
+ */
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Checking this! :)");
+		
+		/*
+	
+		 * Inicializamos todas las variables. Especial atención a numeroActual
+		 * que empezará en 1 para no provocar un producto nulo. Lo mismo para 
+		 * productoImpares que también inicializará a 1 para no provocar producto
+		 * nulo.
+		 */
+		
+		int contadorDeImpares = 0;
+		int numeroActual = 1;
+		int sumaImpares = 0;
+		int productoImpares = 1;
+		
+		// Mientras que no tengamos 10 números impares ejecutaremos el bucle
+		while(contadorDeImpares < 10) {
+			// Si el número resulta impar
+			if ( (numeroActual % 2) != 0) {
+				// Sumamos el número a los anteriores
+				sumaImpares += numeroActual;
+				// Multiplicamos el número por los anteriores
+				productoImpares *= numeroActual;
+				// E incrementamos en 1 el contador de impares
+				contadorDeImpares++;
+			}
+			// Para finalizar, pasamos al siguiente número
+			numeroActual++;
+		}
+		
+		// Escribimos el resultado y se termina el programa
+		System.out.println("La suma es " + sumaImpares + " y el producto es " + productoImpares);
+		
 	}
 
 }
